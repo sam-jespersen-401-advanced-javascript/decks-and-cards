6694b16d1bfef4171df14be3f243abc5c75c9aed
Sam Jespersen <heyheysamj@gmail.com>
2019-10-31 13:35:11 -0700

    card component

---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Card = (_id, name, image) => {
+  return <div key={_id}>
+    <header><h3>{name}</h3></header>
+    <figure>
+      <img src={image} alt={name} />
+    </figure>
+    <p>{name} has {_id}</p>
+  </div>;
+};
+
+Card.propTypes = {
+  _id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+};
+
+export default Card;
